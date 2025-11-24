aa77ce078f95034f8c3d30e0c373c3d48c710dbe
Ifeoma Sylvia Dike <ifeomasylvia.dike@gmail.com>
2025-11-24 03:51:20 +0100

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,7 +1,11 @@
-const Button = ({ title, styles = '' }) => {
+import { ButtonProps } from '@/interfaces';
+
+const Button = ({ title, styles = '' }: ButtonProps) => {
   return (
     <button className={`font-semibold transition-all duration-200 hover:opacity-80 active:scale-95 ${styles}`}>
       {title}
     </button>
   );
 };
+
+export default Button;
