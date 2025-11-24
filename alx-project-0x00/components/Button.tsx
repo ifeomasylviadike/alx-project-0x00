4b3e43cd5fcb18da0ed93f0ca673526545506e0f
Ifeoma Sylvia Dike <ifeomasylvia.dike@gmail.com>
2025-11-24 03:42:16 +0100

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,1 +1,7 @@
-
+const Button = ({ title, styles = '' }) => {
+  return (
+    <button className={`font-semibold transition-all duration-200 hover:opacity-80 active:scale-95 ${styles}`}>
+      {title}
+    </button>
+  );
+};
